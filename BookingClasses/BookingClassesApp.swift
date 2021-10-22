4a79cd154c5c31784200964c13c74ae673f0ee60
Thiago Holanda <unnamedd@gmail.com>
2021-10-22 11:01:28 +0200

    Add basic structure for the iOS project

---
+++ BookingClasses/BookingClassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingClassesApp.swift
+//  BookingClasses
+//
+//  Created by Thiago Holanda on 22.10.21.
+//
+
+import SwiftUI
+
+@main
+struct BookingClassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
