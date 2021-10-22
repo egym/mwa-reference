e53c1f5e425cbdbe1496f3c1146bcad5a44c98e6
Thiago Holanda <unnamedd@gmail.com>
2021-10-22 15:35:58 +0200

    Basic implementation of the SwiftUI App life cycle

--- ios/BookingClasses/SourceCode/BookingClassesApp.swift
+++ ios/BookingClasses/SourceCode/BookingClassesApp.swift
@@ -2,10 +2,15 @@ import SwiftUI
 
 @main
 struct BookingClassesApp: App {
-  @UIAppl
+  
+  @UIApplicationDelegateAdaptor(AppDelegate.self)
+  var appDelegate
+  
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      NavigationView {
+        InitialView()
+      }
     }
   }
 }
