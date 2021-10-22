5e2eee6dd4d3634148ac7813a5296827ccb9842e
Thiago Holanda <unnamedd@gmail.com>
2021-10-22 20:47:14 +0200

    Fix name of the view to be loaded in the BookingClassesApp

--- ios/BookingClasses/SourceCode/BookingClassesApp.swift
+++ ios/BookingClasses/SourceCode/BookingClassesApp.swift
@@ -2,14 +2,19 @@ import SwiftUI
 
 @main
 struct BookingClassesApp: App {
-  
+  @StateObject var bookingManager = BookingManager()
+  private let gym = Gym(name: "EGYM Berlin Office")
+
   @UIApplicationDelegateAdaptor(AppDelegate.self)
   var appDelegate
   
   var body: some Scene {
     WindowGroup {
       NavigationView {
-        InitialView()
+        BookingListView(
+          gym: gym,
+          bookingManager: bookingManager
+        )
       }
     }
   }
