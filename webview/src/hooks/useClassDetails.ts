a3cb4ad48bafbf2d58cef7e11e68b51a121fadb1
alexanderkolomiytsev <alexander.kolomiytsev@gmail.com>
2021-11-05 10:22:13 +0200

    remove ion toast

--- webview/src/hooks/useClassDetails.ts
+++ webview/src/hooks/useClassDetails.ts
@@ -1,20 +1,12 @@
-import { useIonToast } from '@ionic/react';
 import { getClassDetailsById } from '../utils/data';
 import { onClassBookedClick } from '../utils/nativeHandlers';
 
 const useClassDetails = (classId?: string) => {
-  const [presentToast] = useIonToast();
   const classDetails = classId ? getClassDetailsById(classId) : undefined;
 
   const onBookClassClick = () => {
     if (classDetails) {
       console.log('onClassBookedClick', classDetails);
-
-      presentToast({
-        header: 'WEBVIEW TOAST',
-        message: `Sending class details`,
-        duration: 5000
-      });
 
       onClassBookedClick(classDetails)
     }
