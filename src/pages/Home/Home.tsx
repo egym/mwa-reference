6c6e0e8d81828353c35a136c4980c30fb9e35f8e
alexanderkolomiytsev <alexander.kolomiytsev@gmail.com>
2022-08-25 16:13:11 +0300

    http plugin: test different urls 4

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -20,7 +20,7 @@ const Home: FC = () => {
       <IonContent fullscreen style={{ marginBottom: '100px' }}>
         <div style={{ width: '100%', textAlign: 'center', marginTop: '20px' }}>
           <IonText>
-            13 - {window.location.href}
+            14 - {window.location.href}
           </IonText>
         </div>
         <TestAuth />
