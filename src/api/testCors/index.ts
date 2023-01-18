4b2f6d4842ed14ab2965beab3157364bd4d04b45
alexanderkolomiytsev <alexander.kolomiytsev@egym.com>
2023-01-18 19:33:40 +0200

    pass test cors base url for getTestCors

--- src/api/testCors/index.ts
+++ src/api/testCors/index.ts
@@ -2,4 +2,9 @@ import { createApiRequest } from 'src/utils/api';
 
 const TEST_CORS_URL = '/test-cors';
 
-export const getTestCors = createApiRequest<{ message: string }>(TEST_CORS_URL, 'get');
+export const getTestCors = createApiRequest<{ message: string }>(
+  TEST_CORS_URL,
+  'get',
+  undefined,
+  'https://mwa-test-be.herokuapp.com'
+);
