6ed355b290063b8bd1ed73eea36db776608cfc48
Julio <beito_xp@me.com>
2024-02-08 13:00:40 -0600

    Feat: Location Type

---
+++ src/types/locations/index.ts
@@ -0,0 +1,29 @@
+export type Location = {
+  uuid: string;
+  name: string;
+  timezone: string;
+  phone: string;
+  email: string;
+  url: string;
+  address: {
+    addressLine1: string;
+    addressLine2: string;
+    city: string;
+    country: string;
+    postalCode: string;
+    stateOrProvince: string;
+    timezone: string;
+    lat: number;
+    lng: number;
+  };
+  workingHours: {
+    Mon: string;
+    Tue: string;
+    Wed: string;
+    Thu: string;
+    Fri: string;
+    Sat: string;
+    Sun: string;
+  } | null;
+  workingHoursFreeText: string | null;
+};
