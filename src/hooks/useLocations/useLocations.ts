4e42fa13370034493bc81878bf4de6ba85ffc254
Julio <beito_xp@me.com>
2024-02-14 16:47:13 -0600

    Fix: Use location is using createApiRequest

--- src/hooks/useLocations/useLocations.ts
+++ src/hooks/useLocations/useLocations.ts
@@ -1,12 +1,29 @@
 import { useQuery } from 'react-query';
+import { createApiRequest } from 'src/utils/api';
 
-const fetchLocations = async () => {
-  const res = await fetch('https://egymidp.netpulse.com/np/company/children');
-  return res.json();
-};
+const LOCATION_REQUEST = '/children';
+
+const getLocationRequest = createApiRequest<any>(
+  LOCATION_REQUEST,
+  'get',
+  undefined,
+  'https://egymidp.netpulse.com/np/company'
+);
 
 const useLocations = () => {
-  const locationsQuery = useQuery('children', fetchLocations);
+  const locationsQuery = useQuery(
+    ['children'],
+    async () => {
+      return getLocationRequest();
+    },
+    {
+      enabled: true,
+      select: (result) => result.data,
+      staleTime: 0,
+      keepPreviousData: true,
+      refetchOnMount: true,
+    }
+  );
 
   return {
     locationsQuery,
