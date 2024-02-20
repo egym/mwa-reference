bebf91dde0b29626aa4dc0068269d0239d055dea
Julio <beito_xp@me.com>
2024-02-20 10:35:54 -0600

    Fix: Any on hook changed to it's properly type.

--- src/pages/Locations/hooks/useLocationsPage.ts
+++ src/pages/Locations/hooks/useLocationsPage.ts
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import type { IonSearchbarCustomEvent } from '@ionic/core';
+import type { SearchbarChangeEventDetail } from '@ionic/react';
 import type { Location } from '../../../types';
 
 const useLocationsPage = (initialLocations: Location[]) => {
@@ -8,7 +10,7 @@ const useLocationsPage = (initialLocations: Location[]) => {
     setLocationResult(initialLocations);
   }, [initialLocations]);
 
-  const handleSearch = (event: any) => {
+  const handleSearch = (event: IonSearchbarCustomEvent<SearchbarChangeEventDetail>) => {
     const searchText: string = event.target.value || '';
     const regex = new RegExp(searchText, 'i');
     const filteredResults: Location[] = initialLocations.filter((location) => regex.test(location.name));
