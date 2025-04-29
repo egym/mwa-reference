9f938c24b59b06afcd1be56aac4e51f60c13824d
alexanderkolomiytsev <alexander.kolomiytsev@egym.com>
2025-04-29 22:12:34 +0300

    upd capacitor

--- src/pages/BarcodeScanner/hooks/useBarcodeScanner/useBarcodeScanner.ts
+++ src/pages/BarcodeScanner/hooks/useBarcodeScanner/useBarcodeScanner.ts
@@ -1,6 +1,10 @@
-import { useCallback, useEffect, useState } from 'react';
-import { BarcodeScanner, CameraDirection } from '@capacitor-community/barcode-scanner';
-import { logDebug } from '@egym/mwa-logger';
+import { useCallback, useState } from 'react';
+import {
+  CapacitorBarcodeScanner,
+  CapacitorBarcodeScannerCameraDirection,
+  CapacitorBarcodeScannerScanOrientation,
+} from '@capacitor/barcode-scanner';
+import { CapacitorBarcodeScannerTypeHint } from '@capacitor/barcode-scanner/dist/esm/definitions';
 import type { UseBarcodeScannerResultProps } from '../../BarcodeScannerProps';
 
 const useBarcodeScanner = (): UseBarcodeScannerResultProps => {
@@ -8,65 +12,26 @@ const useBarcodeScanner = (): UseBarcodeScannerResultProps => {
   const [error, setError] = useState<boolean>(false);
   const [barcodeActive, setBarcodeActive] = useState<boolean>(false);
 
-  useEffect(() => {
-    BarcodeScanner.prepare();
-
-    return () => {
-      BarcodeScanner.showBackground();
-      BarcodeScanner.stopScan();
-      document.querySelector('body')?.classList.remove('scanner-active');
-    };
-  }, []);
-
   const startScan = useCallback(async () => {
     try {
-      const permissionResult = await BarcodeScanner.checkPermission({ force: true });
+      setBarcodeActive(true);
+      const { ScanResult } = await CapacitorBarcodeScanner.scanBarcode({
+        hint: CapacitorBarcodeScannerTypeHint.ALL,
+        scanOrientation: CapacitorBarcodeScannerScanOrientation.ADAPTIVE,
+        cameraDirection: CapacitorBarcodeScannerCameraDirection.BACK,
+        scanButton: true,
+      });
 
-      logDebug('barcode scanner permission result', permissionResult);
-
-      if (permissionResult.granted) {
-        await BarcodeScanner.hideBackground();
-        document.querySelector('body')?.classList.add('scanner-active');
-
-        setBarcodeActive(true);
-        const result = await BarcodeScanner.startScan({ cameraDirection: CameraDirection.BACK });
-
-        if (result.hasContent) {
-          logDebug('Scanner result', result.content);
-          setScanResult(result.content);
-        } else {
-          logDebug('No content', result);
-          setScanResult('No content');
-        }
-      } else if (permissionResult.denied) {
-        // the user denied permission for good
-        // redirect user to app settings if they want to grant it anyway
-        const c = confirm('If you want to grant permission for using your camera, enable it in the app settings.');
-        if (c) {
-          await BarcodeScanner.openAppSettings();
-        }
-      } else if (permissionResult.asked) {
-        setScanResult('Permission requested. Click "Start Scan" again.');
-      } else {
-        setScanResult('Permission not granted');
-        setError(true);
-      }
-    } catch (e) {
-      setError(true);
-      // @ts-ignore
-      logDebug('Scanning Error', e.toString());
-      // @ts-ignore
-      setScanResult(e.toString());
-    } finally {
-      await BarcodeScanner.showBackground();
-      document.querySelector('body')?.classList.remove('scanner-active');
+      setScanResult(ScanResult);
       setBarcodeActive(false);
+    } catch (error) {
+      setError(true);
+      setBarcodeActive(false);
+      setScanResult('Error scanning barcode');
     }
   }, []);
 
   const stopScan = useCallback(() => {
-    BarcodeScanner.showBackground();
-    BarcodeScanner.stopScan();
     document.querySelector('body')?.classList.remove('scanner-active');
     setBarcodeActive(false);
   }, []);
